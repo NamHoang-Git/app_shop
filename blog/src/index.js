cc33128ca06894c7d6eca29ae68ee7be27b732b6
NamHoang-Git <ngokhoangnam4268@gmail.com>
2024-11-12 15:06:52 +0700

    update

---
+++ blog/src/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { engine } from "express-handlebars"; // "express-handlebars"
+
+import * as path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// 
+app.use(express.urlencoded({
+	extended: true
+}));
+app.use(express.json());
+
+// 
+app.engine("hbs", engine({
+    extname: ".hbs"
+}));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/home", (req, res) => {
+	res.render("home");
+});
+
+app.get("/register", (req, res) => {
+	res.render("register");
+});
+
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+// app.post("/search", (req, res) => {
+// 	console.log(req.body);
+
+// 	res.send('');
+// });
+
+app.listen(3000, () => {
+	console.log("express-handlebars example server listening on: 3000");
+});
---
+++ blog/src/public/js/app.js
@@ -0,0 +1,68 @@
+"use strict";
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const navReg = $('.nav__register');
+const navLogin = $('.nav__login');
+
+const overlay = $('.modal__overlay');
+const driftReg = $('.drift-register');
+const driftLogin = $('.drift-login');
+
+const closeReg = $('.modal__auth-close-reg');
+const closeLogin = $('.modal__auth-close-log');
+
+const footerReg = $('.footer__link-reg');
+const footerLog = $('.footer__link-log');
+
+const shoppingCart = $('.shopping-cart-but')
+
+// Mở modal đăng ký
+navReg.onclick = function() {
+    overlay.classList.add('active');
+    driftReg.classList.add('active');
+}
+
+// Mở modal đăng nhập
+navLogin.onclick = function() {
+    overlay.classList.add('active');
+    driftLogin.classList.add('active');
+}
+
+// Bấm giỏ hàng mở đăng nhập
+shoppingCart.onclick = function() {
+    overlay.classList.add('active');
+    driftLogin.classList.add('active');
+}
+
+
+// Đóng modal và overlay khi nhấp vào overlay
+overlay.onclick = function() {
+    overlay.classList.remove('active');
+    driftReg.classList.remove('active');
+    driftLogin.classList.remove('active');
+}
+
+// Đóng modal đăng ký
+closeReg.onclick = function() {
+    overlay.classList.remove('active');
+    driftReg.classList.remove('active');
+}
+
+// Đóng modal đăng nhập
+closeLogin.onclick = function() {
+    overlay.classList.remove('active');
+    driftLogin.classList.remove('active');
+}
+
+// Chuyển từ modal đăng ký sang modal đăng nhập
+footerReg.onclick = function() {
+    driftLogin.classList.remove('active');
+    driftReg.classList.add('active');
+}
+
+// Chuyển từ modal đăng nhập sang modal đăng ký
+footerLog.onclick = function() {
+    driftReg.classList.remove('active');
+    driftLogin.classList.add('active');
+}
